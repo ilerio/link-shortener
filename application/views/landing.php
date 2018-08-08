9ba99367a873a02682d04576e38e151a507ba6c9
ilerio <ilerioyedele@gmail.com>
2018-08-07 19:36:28 -0600

    small ui change

--- application/views/landing.php
+++ application/views/landing.php
@@ -32,7 +32,7 @@
         width: 100%;
         /* Set the fixed height of the footer here */
         height: 60px;
-        background-color: #f5f5f5;
+        background-color: #e9ecef;
       }
   	</style>
   </head>
